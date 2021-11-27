de01d941641cf7ef1b645285a79c62dc5aa9bc39
andrsmllr <andrsmllr@bananatronics.org>
2021-11-27 17:51:27 +0100

    Implement solution for problem RealGCD (copy from solutions)

--- src/main/scala/problems/RealGCD.scala
+++ src/main/scala/problems/RealGCD.scala
@@ -21,6 +21,30 @@ class RealGCD extends Module {
 
   // Implement below ----------
 
+  // Copied solution again :-(
+  // Can't know how Scala works just from the examples.
+  val x = Reg(UInt())
+  val y = Reg(UInt())
+  val p = RegInit(false.B)
+
+  io.in.ready := !p
+
+  when (io.in.valid && !p) {
+    x := io.in.bits.a
+    y := io.in.bits.b
+    p := true.B
+  } 
+
+  when (p) {
+    when (x > y)  { x := y; y := x } 
+    .otherwise    { y := y - x }
+  }
+
+  io.out.bits  := x
+  io.out.valid := y === 0.U && p
+  when (io.out.valid) {
+    p := false.B
+  }
   // Implement above ----------
 
 }
