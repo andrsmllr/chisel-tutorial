91a45d01d4047b3b4c2437c16b6cb34245793689
andrsmllr <andrsmllr@bananatronics.org>
2021-11-27 19:23:28 +0100

    Implement test for problem Max2

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -2,20 +2,24 @@
 package problems
 
 import chisel3.iotesters.PeekPokeTester
+import scala.util.Random
 
 // Problem:
 //
 // Implement test with PeekPokeTester
 //
 class Max2Tests(c: Max2) extends PeekPokeTester(c) {
+  val r = scala.util.Random
   for (i <- 0 until 10) {
 
     // Implement below ----------
 
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+    val a = r.nextInt(256)
+    val b = r.nextInt(256)
+    poke(c.io.in0, a)
+    poke(c.io.in1, b)
     step(1)
-    expect(c.io.out, 1)
+    expect(c.io.out, if (a > b) a else b)
 
     // Implement above ----------
   }
